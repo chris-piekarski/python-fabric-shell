064e9f7e5681d4d3cbd5433be7d67f6a89fe05d0
Christopher Piekarski <chris@cpiekarsk.com>
2012-06-18 15:27:58 -0600

    More WIP changes.

--- rshell/shell.py
+++ rshell/shell.py
@@ -1,25 +1,34 @@
-from fabric.decorators import hosts
-from fabric.context_managers import settings
+#from fabric.decorators import hosts
+#from fabric.context_managers import settings
 from fabric.api import *
 
-import readline
-
-def shell(host_name):
-	FabricShell().run(host_name)
+def shell(host_name, host_address, user=None):
+	if user != None:
+		env.user = user
+	FabricShell().run(host_name, host_address)
 	
-def issue(command, host_name):
-	FabricShell().run_cmd(command, host_name)
+def issue(command, hosts, user=None):
+	env.hosts = hosts
+	if user != None:
+		env.user = user
+	for h in env.hosts:
+		env.host_string = h
+		FabricShell().run_cmd(command, h)
 
 class FabricShell(object):
-	prompt = 'fabric::> '
+	_prompt = "fabric::{}> "
+	_host_name = 'default'
+	
+	def _make_prompt(self):
+		return self._prompt.format(self._host_name)
 		
-	def run(self, host_name):
-		self._host = host_name
+	def run(self, host_name, host_address):
+		self._host_name = host_name
 		while True:
-			line = raw_input(self.prompt)
+			line = raw_input(self._make_prompt())
 			
 			#
-			# checking first line if we are running a special command
+			# checking first char to see if we are running a special command
 			#
 			if line.strip() == "":
 				continue
@@ -29,16 +38,17 @@ class FabricShell(object):
 					continue
 				
 				if parts[0] == ".sudo":
-					self.run_cmd(line.replace(".sudo", ""), opt="sudo")
+					self.run_cmd(line.replace(".sudo", ""), host_address, opt="sudo")
+				
+			elif line.strip() == "help":
+				print "Type 'quit' or 'exit' for exit the fabric shell"
 				
 			elif line.strip() == "quit" or line.strip() == "exit":
 				break
 			else:
-				self.run_cmd(line)
-
+				self.run_cmd(line, host_address)
 
 	def run_cmd(self, cmd, host_name, **kwargs):
-		env.user = env.local_user
 		with settings(host_string = host_name):
 			if kwargs.has_key('opt') and kwargs['opt'] == "sudo":
 				sudo(cmd)
